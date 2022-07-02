63b63c9ae618a3cb1daefc1815fbc730fb1be41d
IvanAmamiya <106081022+IvanAmamiya@users.noreply.github.com>
2022-07-02 15:25:11 +0800

    config 0.0.1

--- Main.py
+++ Main.py
@@ -1,9 +1,13 @@
 from flask import Flask
+import config
+
 app = Flask(__name__)
+
+app.config.from_object(config)
 @app.route('/index/')
 
 def index():
-  return {"username":"114514"}
+  return {"username":"您"}
 
 if(__name__=='__main__'):
-  app.run(debug = True)
+  app.run(debug = False)
