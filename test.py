5eb308e411576bb5b36ed7d38a5a885d720cba42
IvanAmamiya <106081022+IvanAmamiya@users.noreply.github.com>
2022-07-04 21:36:56 +0800

    0.0.3.1

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 def log(func):
     def wrapper(name):
-        print('before calling'),func.__name__
+        print('before calling',func.__name__)
         func(name)
-        print('end calling'),func.__name__
+        print('end calling',func.__name__)
     return wrapper
 @log
 def morning(name):
     print('hello',name)
-if(__name__=='__main__'):
+if __name__=='__main__':
     morning('NNN')
